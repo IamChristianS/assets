62b3edb94a8f7d650373af2c766dc105c4eda2c4
Christian Santangelo <95893289+IamChristianS@users.noreply.github.com>
2024-03-06 12:53:38 -0600

    testing more config

--- ruffle/config.js
+++ ruffle/config.js
@@ -2,6 +2,13 @@ window.RufflePlayer = window.RufflePlayer || {};
 window.RufflePlayer.config = {
     "showSwfDownload": true,
     "backgroundColor":  "#000",
-    "salign": "",
-    "scale": "noBorder"
 };
+
+var style = document.createElement('style');
+style.innerHTML = `
+    player {
+        width: 100vw;
+        height: 100vh;
+    }
+`;
+document.head.appendChild(style);
