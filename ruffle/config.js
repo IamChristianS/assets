30e0c48abad7876259d09a4a07761c244eb0ccb7
Christian Santangelo <95893289+IamChristianS@users.noreply.github.com>
2024-03-06 12:43:47 -0600

    testing new config

---
+++ ruffle/config.js
@@ -0,0 +1,7 @@
+window.RufflePlayer = window.RufflePlayer || {};
+window.RufflePlayer.config = {
+    "showSwfDownload": true,
+    "backgroundColor":  "#000",
+    "salign": "",
+    "scale": "noBorder"
+};
